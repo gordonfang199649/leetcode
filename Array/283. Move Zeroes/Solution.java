4d9b9b8de4542dc246b19940cae1d5e65cbac9e6
Gordon <just8574322589@gmail.com>
2021-06-11 01:43:53 +0800

    add: 283. Move Zeroes

---
+++ Array/283. Move Zeroes/Solution.java
@@ -0,0 +1,41 @@
+/*
+Problem Solver: Gordon Fang
+Method: Two Pointer Method
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Move Zeroes.
+Memory Usage: 39 MB, less than 85.85% of Java online submissions for Move Zeroes.
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+*/
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] != 0){
+                int temp = nums[j];
+                nums[j++] = nums[i];
+                nums[i] = temp;
+            }
+        }
+    }
+}
