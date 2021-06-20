49977f623193fa6fae4a28f629685e9182d7a1cf
Gordon <just8574322589@gmail.com>
2021-06-21 01:51:34 +0800

    add: 203. Remove Linked List Elements (Dummy Node)

---
+++ LinkedList/203. Remove Linked List Elements/Solution.java
@@ -0,0 +1,61 @@
+/**
+Problem Solver: gordon fang
+Method: Iterative Method
+Time Complexity: O(n)
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Remove Linked List Elements.
+Memory Usage: 40.1 MB, less than 35.21% of Java online submissions for Remove Linked List Elements.
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50
+
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null) {
+            return head;
+        }
+        
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode current = dummy;
+        
+        while(current.next != null) {
+            if(current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        
+        return dummy.next;
+    }
+}
