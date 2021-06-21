041b5ad6b37192439631a8305e86ab17fe8c358b
Gordon <just8574322589@gmail.com>
2021-06-22 00:00:17 +0800

    docs: 補充解題基本資訊

--- LinkedList/24. Swap Nodes in Pairs/Solution.java
+++ LinkedList/24. Swap Nodes in Pairs/Solution.java
@@ -1,4 +1,9 @@
 /**
+Problem Solver: gordon fang
+Method: Iterative Method
+Time Complexity: O(n)
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Swap Nodes in Pairs.
+Memory Usage: 36.4 MB, less than 72.21% of Java online submissions for Swap Nodes in Pairs.
 Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
 
  
@@ -23,13 +28,13 @@ Constraints:
 The number of nodes in the list is in the range [0, 100].
 0 <= Node.val <= 100
 
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+* Definition for singly-linked list.
+* public class ListNode {
+*     int val;
+*     ListNode next;
+*     ListNode(int x) { val = x; }
+* }
+*/
 
 class Solution {
     public ListNode swapPairs(ListNode head) {
